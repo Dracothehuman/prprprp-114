ec43c3267dc490c4518a5f5ece4f60fd63f70762
Dracothehuman <80511020+Dracothehuman@users.noreply.github.com>
2021-10-30 13:01:52 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+mouthX = 0;
+mouthY = 0;
+
+
+
+function preload(){
+    creeper_aw_man = loadImage("https://i.postimg.cc/tCrHG2Rf/220px-Pikachu-artwork-for-Pok-mon-Red-and-Blue-webp.png");
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save("mumbo_jumbo.png");
+}
